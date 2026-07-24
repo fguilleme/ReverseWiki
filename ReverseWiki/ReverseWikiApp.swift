cad66549d22eb6953f2fffb7b7171c5d444f458b
François Guillemé <francois@guilleme.net>
2026-07-24 22:17:06 +0200

    Initial Commit

---
+++ ReverseWiki/ReverseWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReverseWikiApp.swift
+//  ReverseWiki
+//
+//  Created by François Guillemé on 24/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct ReverseWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReverseWikiTests/ReverseWikiTests.swift
@@ -0,0 +1,19 @@
+//
+//  ReverseWikiTests.swift
+//  ReverseWikiTests
+//
+//  Created by François Guillemé on 24/07/2026.
+//
+
+import Testing
+@testable import ReverseWiki
+
+struct ReverseWikiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
